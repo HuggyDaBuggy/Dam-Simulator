d7a35c9212f58ef88ae29c6a9143771c0b772b2c
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-09-11 14:58:21 +0800

    DAM YAYYYAYAYAYYAY

--- App/JS/game.js
+++ App/JS/game.js
@@ -1,17 +1,24 @@
-function game() {
-  var points = 0;
-  var dam = 0;
-  var random = 0;
-  var alive = "true";
-  while (alive === "true") {
-    if (dam === 0) {
-      document.getElementById("dam").setAttribute("src", "../Images/Dams/dam0.png")
-      var random = Math.floor(Math.random() * 10) + 1;
-      if (random === 1) {
-        var alive = "false"
-      }
+var points = 0;
+var dam = 0;
+var random = 0;
+var alive = "true";
+
+function point() {
+  if (dam == 0) {
+    document.getElementById("dam").setAttribute("src", "../Images/Dams/dam0.jfif")
+    var random = Math.floor(Math.random() * 10) + 1;
+    if (random == 1) {
+      alert("Your dam broke lmao")
+      window.location.replace("home.html")
+    }
+    if (random != 1) {
+      points = points + 1;
+      document.getElementById("points").innerHTML = points;
     }
   }
-  alert("Your dam broke lmao")
-  window.location.replace("home.html")
+}
+
+function load() {
+  document.getElementById("dam").setAttribute("src", "../Images/Dams/dam0.jfif")
+  document.getElementById("points").innerHTML = "0";
 }
