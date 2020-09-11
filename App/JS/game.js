ab48647090146f78208db173febc5ac3939dc398
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-09-11 18:57:51 +0800

    DAMS!

--- App/JS/game.js
+++ App/JS/game.js
@@ -1,17 +1,17 @@
 var points = 0;
-var dam = 0;
+var dam = 1;
 var random = 0;
 var cost = 5;
 
 function point() {
   document.getElementById("dam").setAttribute("src", "../Images/Dams/dam" + dam + ".png")
-  var random = Math.floor(Math.random() * 10) + 1;
+  var random = Math.floor(Math.random() * (dam * 10)) + 1;
   if (random == 1) {
     alert("Your dam broke lmao")
-    window.location.replace("home.html")
+    death()
   }
   if (random != 1) {
-    points = points + 1;
+    points = points + (dam * 1);
     document.getElementById("points").innerHTML = "Dam Points: " + points;
   }
 }
@@ -19,15 +19,27 @@ function point() {
 function load() {
   document.getElementById("dam").setAttribute("src", "../Images/Dams/dam" + dam + ".png")
   document.getElementById("points").innerHTML = "Dam Points: " + points;
+  document.getElementById("dam-text").innerHTML = "Level " + dam + " Dam";
 }
 
 function buy() {
   if (points == cost || points > cost) {
     dam = dam + 1;
     points = points - cost;
-    document.getElementById("points").innerHTML = points;
-  }
-  else {
+    cost = cost * dam;
+    document.getElementById("points").innerHTML = "Dam Points: " + points;
+    document.getElementById("dam-text").innerHTML = "Level " + dam + " Dam"; 
+    document.getElementById("dam").setAttribute("src", "../Images/Dams/dam" + dam + ".png")
+    document.getElementById("buy-dam").setAttribute("src", "Upgrade Dam(" + cost + " Dam points)")
+  } else {
     alert("Your too poor lmao")
   }
+}
+
+function death() {
+  points = 0;
+  dam = 1;
+  random = 0;
+  cost = 5;
+  window.location.replace("home.html")
 }
